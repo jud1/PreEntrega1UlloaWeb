7e08c5aa70fc67ba9b2751bff9f73fe4738d796b
Jose Ulloa <contacto@jud.cl>
2022-09-22 21:42:16 -0300

    PreEntrega2 Ulloa

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,17 +1,15 @@
 /* Funciones ejecutadas al cargar todo el dom */
-window.onload = function(){
+window.onload = function () {
 
-   console.log('a');
+    /* Scroll suave */
+    document.querySelectorAll('[smooth-scroll]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-   /* Scroll suave */
-   document.querySelectorAll('[smooth-scroll]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-          e.preventDefault();
-  
-          document.querySelector(this.getAttribute('href')).scrollIntoView({
-              behavior: 'smooth'
-          });
-      });
-  });
-
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    
 };
